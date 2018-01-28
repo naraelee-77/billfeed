440ff3b0a3d67925781eba8d62283ace4c020187
Narae Lee <Narae@MacBook-Air-29.local>
2018-01-27 21:29:19 -0500

    Pulling bills from API

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -13,6 +13,8 @@
 
 <script>
 import Modal from '@/components/Modal.vue'
+require('dotenv').config()
+var fetch = require('node-fetch')
 
 export default {
   name: 'Card',
@@ -23,17 +25,42 @@ export default {
     card: {
       title: String,
       intro: String,
-      text: String
+      text: String,
+      category: String,
+      dateIntroduced: String,
+      houseOrSenate: String
     }
   },
   data: function () {
     return {
     }
   },
+
+  created: function () {
+    this.getBillInfo()
+  },
+
   methods: {
     handleClick: function (event) {
       alert('Hi')
       return 'block'
+    },
+
+    getBillInfo: function () {
+      var propub = 'https://api.propublica.org/congress/v1/115/bills/hr4881'
+      fetch(propub, { headers: {
+        'X-API-Key': 'kTURrPH8Awsjo6H8zd7JSEqfyFrxvPO0ubGmbHl6'
+      }})
+        .then((res) => res.json())
+        .then((json) => {
+          var results = json.results[0]
+          this.card.title = results.short_title
+          this.card.intro = results.title
+          this.card.text = results.summary
+          this.card.category = results.primary_subject
+          this.card.houseOrSenate = results.bill_type
+          this.card.dateIntroduced = results.introduced_date
+        })
     }
   }
 }
